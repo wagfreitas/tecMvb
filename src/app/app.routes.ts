070405999bd8f7d78ecbcb61e290f80d7dad3bd3
Wagner Alves <wagfreitas@hotmail.com>
2023-04-19 21:33:33 -0300

    feat: inserido componentes que serao retirados depois

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,4 +5,8 @@ export const routes: Routes = [
     path: '',
     loadChildren: () => import('./components/tabs/tabs.routes').then((m) => m.routes),
   },
+  {
+    path: 'usuario',
+    loadComponent: () => import('./components/usuario/usuario.page').then( m => m.UsuarioPage)
+  },
 ];
